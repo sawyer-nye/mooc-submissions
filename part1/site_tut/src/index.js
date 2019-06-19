30a49f1ef9a222f493439c8df14f3c49948a5673
Sawyer Nye <sawyer.nye@live.com>
2019-06-19 13:59:21 -0400

    Commit progression on part 1c site example code

--- part1/site_tut/src/index.js
+++ part1/site_tut/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+//import React from 'react'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+/*
+// props passed to the component are directly destructured into name and age
+const Hello = ({ name, age }) => {
+    // defines func bornYear that returns year - age
+    const bornYear = () => new Date().getFullYear() - age
+
     return (
         <div>
-            <p>Hello {props.name}, you are {props.age} years old</p>
+            <p>Hello {name}, you are {age} years old</p>
+            <p>So you were probably born {bornYear()}</p>
         </div>
     );
 }
@@ -21,5 +28,36 @@ const App = () => {
         </>
     );
 }
+*/
+// SECTION LEFT OFF: passing state to child components
+const Display = (props) => {
+    return (
+        <div>{props.counter}</div>
+    );
+}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const App = (props) => {
+    const [ counter, setCounter ] = useState(0);
+
+    // returns a func that sets counter to specified value
+    // first func call: configures second function
+    const setToValue = (value) => () => setCounter(value);
+
+    return (
+        <div>
+            <div>{counter}</div>
+            <button onClick={setToValue(counter + 1)}>
+                plus
+            </button>
+            <button onClick={setToValue(0)}>
+                zero
+            </button>
+        </div>
+            
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
